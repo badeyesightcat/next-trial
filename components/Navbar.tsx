a5aa4745602f256a34845f1247cd45445a913fcb
Beom Mee <whitebigbabybird@gmail.com>
2024-05-28 13:59:55 +0900

    modify navbar menus

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
 
       <ul className="flex items-center gap-4">
         <li>
-          <Link href={"/login"}>Login</Link>
+          <Link href={"/login"}>Issue board</Link>
         </li>
         <li>
-          <Link href={"/signup"}>Sign up</Link>
+          <Link href={"/signup"}>Profile</Link>
         </li>
       </ul>
     </nav>
