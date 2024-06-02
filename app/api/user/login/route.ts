13fed79ba5cc7a2c3c0039d19b61be9722c6331c
Beom Mee <whitebigbabybird@gmail.com>
2024-06-03 02:13:00 +0900

    Update route.ts

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -2,12 +2,45 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 connect();
 
-export async function POST(request, response) {
+export async function POST(request: NextRequest, response: NextResponse) {
   try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+  
+    // check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({
+        error: 'User does not exist.'
+      }, { status: 400 });
+    }
+  
+    // check if there is no password matched
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({
+        error: 'Invalid password'
+      }, {status: 400});
+    }
+  
+    // create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+  
+    // create token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: '1d' });
+    const response = NextResponse.json({ message: 'Login successful', success: true }, { status: 200 });
+    response.cookies.set('token', token, { httpOnly: true });
     
+    return response;
   } catch (error) {
     return NextResponse.json({ message: 'Login failed' });    
   }
