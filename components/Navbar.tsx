5fb616a77e0b73ee0889390ada9ed6485fb9a294
Beom Mee <whitebigbabybird@gmail.com>
2024-06-03 10:57:20 +0900

    Add Logout button and styles related to the nav menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import LogoutButton from "./LogoutButton";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center gap-10">
       <Logo />
 
-      <ul className="flex items-center gap-4">
+      <ul className="flex items-center gap-4 flex-1">
         <li>
           <Link href={"/issue-board"}>Issue board</Link>
         </li>
@@ -15,6 +16,8 @@ const Navbar = () => {
           <Link href={"/profile"}>Profile</Link>
         </li>
       </ul>
+
+      <LogoutButton />
     </nav>
   );
 };
