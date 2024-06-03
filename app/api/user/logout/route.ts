079fb2eec74799a19086e39fd4701bafae9d5a21
Beom Mee <whitebigbabybird@gmail.com>
2024-06-03 14:07:47 +0900

    Add try/catch block for logout endpoint

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = NextResponse.json({
-    message: 'Log out successfully',
-    success: true
-  }, {
-    status: 200
-  });
-
-  response.cookies.set('token', '', {
-    httpOnly: true,
-    expires: new Date(0)
-  });
-
-  return response;
+  try {
+    const response = NextResponse.json({
+      message: 'Log out successfully',
+      success: true
+    }, {
+      status: 200
+    });
+  
+    response.cookies.set('token', '', {
+      httpOnly: true,
+      expires: new Date(0)
+    });
+  
+    return response;    
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
