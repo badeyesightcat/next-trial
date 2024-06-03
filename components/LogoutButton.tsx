f7e6e27656120047ce18b017e3a2b8b190d7ddb9
Beom Mee <whitebigbabybird@gmail.com>
2024-06-03 10:58:35 +0900

    Add logout button component

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
+
+const LogoutButton = () => {
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("/api/user/logout");
+      console.log(response);
+      router.push("/login");
+      toast.success("Logged out successfully");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  return (
+    <button
+      onClick={handleLogout}
+      className="mr-8 px-4 py-2 border-4 border-transparent hover:border-[#01bf64] hover:border-dashed rounded-lg"
+    >
+      Log out
+    </button>
+  );
+};
+
+export default LogoutButton;
