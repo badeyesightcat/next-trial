9716e59e1d529843e9c1cba65ca4a6b983e0ca51
Beom Mee <whitebigbabybird@gmail.com>
2024-06-08 22:49:15 +0900

    add logic for getting user's information

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 type UserProfileProps = {
   params: {
@@ -9,10 +9,35 @@ type UserProfileProps = {
   searchParams: object;
 };
 
-const UserProfile = ({ params }: UserProfileProps) => {
-  const { id } = params;
+type UserInfo = {
+  username?: string;
+  email?: string;
+  id?: string;
+};
 
-  return <div>UserProfile of {id}</div>;
+const UserProfile = ({ params }: UserProfileProps) => {
+  const [userInfo, setUserInfo] = useState<UserInfo>({});
+
+  const getuserInfo = async () => {
+    const response = await fetch("/api/user/me");
+    const {
+      data: { userInfo },
+    } = await response.json();
+    console.log(userInfo);
+    setUserInfo(userInfo);
+  };
+
+  return (
+    <>
+      <button
+        className="m-4 p-2 bg-slate-400 rounded-lg text-white"
+        onClick={getuserInfo}
+      >
+        get information of the user
+      </button>
+      <div>UserProfile of {userInfo.username}</div>
+    </>
+  );
 };
 
 export default UserProfile;
