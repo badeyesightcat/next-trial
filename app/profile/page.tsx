678e6fd1a8726e2b45aa185dbed3f83b4c7f86b6
Beom Mee <whitebigbabybird@gmail.com>
2024-06-13 14:18:17 +0900

    add logic for getting user information and redirect to the each user's profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const router = useRouter();
+  const getUserInfo = async () => {
+    const response = await fetch("/api/user/me");
+    const {
+      data: { user },
+    } = await response.json();
+    console.log(user);
+    router.push(`/profile/${user.username}`);
+  };
+  return (
+    <div>
+      <h1>Profile</h1>
+      <button
+        className="m-4 p-2 bg-slate-400 rounded-lg text-white"
+        onClick={getUserInfo}
+      >
+        get information of the user
+      </button>
+    </div>
+  );
 };
 
 export default Profile;
