24198d7cbf7f53e798a9ca85307379afc2b44d75
Beom Mee <whitebigbabybird@gmail.com>
2024-06-13 13:48:01 +0900

    Add quering for specific values after finding wanted data db and connect method and use it and

--- app/api/user/me/route.ts
+++ app/api/user/me/route.ts
@@ -1,15 +1,22 @@
+import { connect } from "@/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+
+connect();
 
 export async function GET(request: NextRequest) {
   try {
     const userInfo = getDataFromToken(request);
-    return NextResponse.json({
-      message: "Successfully got user id",
-      data: { userInfo },
-      status: 200,
-    });
+    const user = await User.findOne({ _id: userInfo.id }).select("-password");
+    return NextResponse.json(
+      {
+        message: "User found successfully",
+        data: { user },
+      },
+      { status: 200 }
+    );
   } catch (error: any) {
-    return NextResponse.json({ error: error.message });
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
